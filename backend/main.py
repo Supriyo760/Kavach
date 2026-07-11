f663b7dbfec39bd71ae569c12cd9e8060a152069
Supriyo Chowdhury <Supriyo760@users.noreply.github.com>
2026-07-11 19:59:46 +0530

    Fix CORS config

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # In prototype mode, allow all origins
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
